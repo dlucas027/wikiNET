b5fc9fd6d550f22e596eacef1f019321ebd8ac4c
Matheus <dlucas.el.patron@gmail.com>
2025-03-27 16:54:31 -0300

    refactor

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function toggleSidebar() {
-    const sidebar = document.getElementById("sidebar");
+//Mecanismo do menu
+function toggleSidebar() {  //função chamada toggleSidebar será executada quando o botão ☰ for clicado no HTML 
+    const sidebar = document.getElementById("sidebar"); //pega um elemento HTML com o atributo
+
+    // Alterna (liga/desliga) a classe "collapsed"
+    // Se tiver, remove; se não tiver, adiciona
     sidebar.classList.toggle("collapsed");
 }
